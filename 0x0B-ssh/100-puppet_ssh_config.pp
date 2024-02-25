caca3aa13a05b5a16f771f938b6e6241bd094463
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-02-25 23:44:01 +0200

    Fix task 100 v3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
-# Set the config of a ssh_file
+# Set the config of ssh file
 
 file_line { 'Turn off passwd auth':
+  ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
