0b735e7508ed2f29c2a6df37a24f99f0b10916d6
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-04-23 01:09:21 +0200

    Edit the task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,15 +7,12 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(f"{url}users/{user_id}").json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
-        fieldnames = ["userId", "username", "completed", "title"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-        writer.writeheader()
-        writer.writerows([
-            {"userId": user_id, "username": username, "completed": t.get("completed"), "title": t.get("title")}
-            for t in todos
-        ])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
